b6e45d9e02331bd87ce148041737d379833332f8
Nile78 <elin.ohlson.78@gmail.com>
2023-01-03 12:13:26 +0100

    FourCalculatorProjektTKTTS_Stockholm

---
+++ src/CalculationBackEndOperators.java
@@ -0,0 +1,33 @@
+public class CalculationBackEndOperators {
+
+    public double operatorPlus(double firstNumber, double secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public double operatorMinus(double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+
+    }
+
+    public double operatorDivision(double firstNumber, double secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public double operatorMultiplication(double firstNumber, double secondNumber) {
+        return firstNumber * secondNumber;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- src/CalculatorProjektTKTTS_Stockholm.java
+++ src/CalculatorProjektTKTTS_Stockholm.java
@@ -4,11 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CalculatorProjektTKTTS_Stockholm implements ActionListener {
-
-    Calculation calculation = new Calculation();
-    double firstNumber, secondNumber, summa;
+    CalculationBackEndOperators calculationBackEndOperators = new CalculationBackEndOperators();
+    private double firstNumber, secondNumber, summa;
     private String operation;
-    private JButton buttonZero = new JButton("0"), buttonOne = new JButton("1"), buttonTwo = new JButton("2"),
+    private final JPanel panel = new JPanel();
+    private final JFrame frame = new JFrame();
+    private final JTextArea textArea = new JTextArea();
+    private final JButton  buttonZero = new JButton("0"), buttonOne = new JButton("1"), buttonTwo = new JButton("2"),
             buttonThree = new JButton("3"), buttonFour = new JButton("4"), buttonFive = new JButton("5"),
             buttonSix = new JButton("6"), buttonSeven = new JButton("7"), buttonEight = new JButton("8"),
             buttonNine = new JButton("9"), buttonPlus = new JButton("+"), buttonMinus = new JButton("-"),
@@ -16,206 +18,205 @@ public class CalculatorProjektTKTTS_Stockholm implements ActionListener {
 
         CalculatorProjektTKTTS_Stockholm() {
 
-
-            JTextArea textArea = new JTextArea();
-
-            JPanel panel = new JPanel();
-            panel.setVisible(true);
-            panel.setBackground(Color.BLACK);
-            panel.setLayout(new GridLayout(4,4,5,5));
-
-            JFrame frame = new JFrame();
-            frame.setSize(300, 370);
+            frame.add(setUpPanel());
+            frame.setSize(300, 400);
             frame.setVisible(true);
-            frame.setTitle("Calculator");
-            frame.add(panel);
+            frame.setTitle("My First Calculator");
 
-            buttonOne.setBackground(Color.LIGHT_GRAY);
-            buttonTwo.setBackground(Color.LIGHT_GRAY);
-            buttonThree.setBackground(Color.LIGHT_GRAY);
-            buttonFour.setBackground(Color.LIGHT_GRAY);
-            buttonFive.setBackground(Color.LIGHT_GRAY);
-            buttonSix.setBackground(Color.LIGHT_GRAY);
-            buttonSeven.setBackground(Color.LIGHT_GRAY);
-            buttonEight.setBackground(Color.LIGHT_GRAY);
-            buttonNine.setBackground(Color.LIGHT_GRAY);
-            buttonZero.setBackground(Color.LIGHT_GRAY);
-            buttonPlus.setBackground(Color.LIGHT_GRAY);
-            buttonMinus.setBackground(Color.LIGHT_GRAY);
-            buttonDivision.setBackground(Color.LIGHT_GRAY);
-            buttonMultiplication.setBackground(Color.LIGHT_GRAY);
+            buttonZeroSetUP(); buttonOneSetUp(); buttonTwoSetUp(); buttonThreeSetUp(); buttonFourSetUp();
+            buttonFiveSetUp(); buttonSixSetUp(); buttonSevenSetUP(); buttonEightSetUp();buttonNineSetUp();
+            buttonPlusSetUp(); buttonMinusSetUp(); buttonDivisionSetUp(); getButtonMultiplicationSetUp();
+
             buttonSumma.setBackground(Color.LIGHT_GRAY);
-
-            panel.add(textArea);
-            panel.add(buttonOne);
-            panel.add(buttonTwo);
-            panel.add(buttonThree);
-            panel.add(buttonFour);
-            panel.add(buttonFive);
-            panel.add(buttonSix);
-            panel.add(buttonSeven);
-            panel.add(buttonEight);
-            panel.add(buttonNine);
-            panel.add(buttonZero);
-            panel.add(buttonPlus);
-            panel.add(buttonMinus);
-            panel.add(buttonMultiplication);
-            panel.add(buttonDivision);
-            panel.add(buttonSumma);
-
-
-             buttonZero.addActionListener(new ActionListener() {
+            buttonSumma.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText() + buttonZero.getText();
-                    textArea.setText(number);
+                    String answer;
+
+                    secondNumber = Double.parseDouble(textArea.getText());
+                    if (operation == "+") {
+                        answer = String.format("%.2f", calculationBackEndOperators.operatorPlus
+                                (firstNumber, secondNumber));
+                        textArea.setText(answer);
+                    }
+
+                    if (operation == "-") {
+                        answer = String.format("%.2f", calculationBackEndOperators.operatorMinus
+                                (firstNumber, secondNumber));
+                        textArea.setText(answer);
+
+                    } else if (operation == "*") {
+                        answer = String.format("%.2f", calculationBackEndOperators.operatorMultiplication
+                                (firstNumber, secondNumber));
+                        textArea.setText(answer);
+                    }
+
+                    if (operation == "/") {
+                        if (secondNumber == 0) {
+                            textArea.setText("Cannot divide by zero");
+                        } else {
+                            answer = String.format("%.2f", calculationBackEndOperators.operatorDivision
+                                    (firstNumber, secondNumber));
+                            textArea.setText(answer);
+                        }
+                    }
                 }
             });
+        }
 
-           buttonOne.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText()+buttonOne.getText();
-                    textArea.setText(number);
-                }
-            });
+    public JPanel setUpPanel() {
+        panel.add(textArea); panel.setLayout(new GridLayout(4,4,5,5));panel.setBackground(Color.BLACK);panel.add(buttonZero);panel.add(buttonZero);
+        panel.add(buttonOne);panel.add(buttonTwo);panel.add(buttonThree);panel.add(buttonFour);panel.add(buttonFive);
+        panel.add(buttonSix);panel.add(buttonSeven);panel.add(buttonEight);panel.add(buttonNine);panel.add(buttonPlus);
+        panel.add(buttonMinus);panel.add(buttonDivision);panel.add(buttonMultiplication);panel.add(buttonSumma);
+        return panel;
+    }
 
-            buttonTwo.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText() + buttonTwo.getText();
-                    textArea.setText(number);
-                }
-            });
-
-            buttonThree.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText() + buttonThree.getText();
-                    textArea.setText(number);
-                }
-            });
-
-            buttonFour.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText() + buttonFour.getText();
-                    textArea.setText(number);
-                }
-            });
-
-            buttonFive.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText() + buttonFive.getText();
-                    textArea.setText(number);
-                }
-            });
-
-            buttonSix.addActionListener(new ActionListener() {
+    public JButton buttonZeroSetUP() {
+        buttonZero.setBackground(Color.LIGHT_GRAY);
+        buttonZero.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonZero.getText();
+                textArea.setText(number);
+            }
+        });
+        return buttonZero;
+    }
+    public JButton buttonOneSetUp () {
+        buttonOne.setBackground(Color.LIGHT_GRAY);
+        buttonOne.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonOne.getText();
+                textArea.setText(number);
+            }
+        }); return buttonOne;
+    }
+    public JButton buttonTwoSetUp () {
+        buttonTwo.setBackground(Color.LIGHT_GRAY);
+        buttonTwo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonTwo.getText();
+                textArea.setText(number);
+            }
+        }); return buttonTwo;
+    }
+    public JButton buttonThreeSetUp () {
+        buttonThree.setBackground(Color.LIGHT_GRAY);
+        buttonThree.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonThree.getText();
+                textArea.setText(number);
+            }
+        }); return buttonThree;
+    }
+    public JButton buttonFourSetUp() {
+        buttonFour.setBackground(Color.LIGHT_GRAY);
+        buttonFour.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonFour.getText();
+                textArea.setText(number);
+            }
+        }); return buttonFour;
+    }
+    public JButton buttonFiveSetUp() {
+        buttonFive.setBackground(Color.LIGHT_GRAY);
+        buttonFive.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonFive.getText();
+                textArea.setText(number);
+            }
+        }); return buttonFive;
+   }
+    public JButton buttonSixSetUp() {
+            buttonSix.setBackground(Color.LIGHT_GRAY);
+        buttonSix.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     String number = textArea.getText() + buttonSix.getText();
                     textArea.setText(number);
                 }
-            });
-
-            buttonSeven.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText() + buttonSeven.getText();
-                    textArea.setText(number);
-                }
-            });
-
-            buttonEight.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText() + buttonEight.getText();
-                    textArea.setText(number);
-                }
-            });
-
-            buttonNine.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String number = textArea.getText() + buttonNine.getText();
-                    textArea.setText(number);
-                }
-            });
-
-            buttonPlus.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    firstNumber = Double.parseDouble(textArea.getText());
-                    textArea.setText("");
-                    operation = "+";
-                }
-            });
-
-            buttonMinus.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    firstNumber = Double.parseDouble(textArea.getText());
-                    textArea.setText("");
-                    operation = "-";
-                }
-            });
-
-            buttonDivision.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    firstNumber = Double.parseDouble(textArea.getText());
-                    textArea.setText("");
-                    operation = "/";
-                }
-            });
-
-            buttonMultiplication.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    firstNumber = Double.parseDouble(textArea.getText());
-                    textArea.setText("");
-                    operation = "*";}
-            });
-
-
-            buttonSumma.addActionListener(new ActionListener() {
-                @Override
-
-                   public void actionPerformed(ActionEvent e) {
-                    String answer;
-
-                    secondNumber = Double.parseDouble(textArea.getText());
-                    if (operation == "+") {
-                        summa = calculation.operatorPlus(firstNumber, secondNumber);
-                        answer = String.format("%.2f", summa);
-                        textArea.setText(answer);
-
-                    }else if (operation == "-"){
-                        summa = calculation.operatorMinus(firstNumber, secondNumber);
-                        answer = String.format("%.2f", summa);
-                        textArea.setText(answer);
-
-                    }else if (operation == "*"){
-                        summa = calculation.operatorMultiplication(firstNumber, secondNumber);
-                        answer = String.format("%.2f", summa);
-                        textArea.setText(answer);
-
-                    } else if (operation == "/"){
-                        summa = calculation.operatorDivision(firstNumber, secondNumber);
-                        if (secondNumber == 0) {
-                            textArea.setText("Cannot divide by zero");
-                        } else {
-                            summa = calculation.operatorDivision(firstNumber, secondNumber);
-                            answer = String.format("%.2f", summa);
-                            textArea.setText(answer);
-                    }
-
-                }}
-            });
-        }
-
+            }); return buttonSix;
+    }
+    public JButton buttonSevenSetUP () {
+        buttonSeven.setBackground(Color.LIGHT_GRAY);
+        buttonSeven.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonSeven.getText();
+                textArea.setText(number);
+            }
+        }); return buttonSeven;
+    }
+    public JButton buttonEightSetUp() {
+        buttonEight.setBackground(Color.LIGHT_GRAY);
+        buttonEight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonEight.getText();
+                textArea.setText(number);
+            }
+        }); return buttonOne;
+    }
+    public JButton buttonNineSetUp() {
+        buttonNine.setBackground(Color.LIGHT_GRAY);
+        buttonNine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = textArea.getText() + buttonNine.getText();
+                textArea.setText(number);
+            }
+        }); return buttonNine;
+    }
+    public JButton buttonPlusSetUp() {
+        buttonPlus.setBackground(Color.LIGHT_GRAY);
+        buttonPlus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstNumber = Double.parseDouble(textArea.getText());
+                textArea.setText("");
+                operation = "+";
+            }
+        }); return buttonPlus;
+    }
+    public JButton buttonMinusSetUp() {
+        buttonMinus.setBackground(Color.LIGHT_GRAY);
+        buttonMinus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstNumber = Double.parseDouble(textArea.getText());
+                textArea.setText("");
+                operation = "-";
+            }
+        }); return buttonMinus;
+    }
+    public JButton buttonDivisionSetUp() {
+        buttonDivision.setBackground(Color.LIGHT_GRAY);
+        buttonDivision.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstNumber = Double.parseDouble(textArea.getText());
+                textArea.setText("");
+                operation = "/";
+            }
+        }); return buttonThree;
+    }
+    public JButton getButtonMultiplicationSetUp() {
+        buttonMultiplication.setBackground(Color.LIGHT_GRAY);
+        buttonMultiplication.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstNumber = Double.parseDouble(textArea.getText());
+                textArea.setText("");
+                operation = "*";
+            }
+        }); return buttonMultiplication;
+    }
     @Override
         public void actionPerformed(ActionEvent e) {
         }
